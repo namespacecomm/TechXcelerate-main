9e5c76dd7037e4eae565597037f2414f32d7943e
Pradeepto Sarkar <psrcofficial10@gmail.com>
2024-10-19 15:51:08 +0530

    namespace linked to website

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = (props) => {
   return (
     <section id="about">
       <Container>
-        <Row style={{ marginBottom: '50px'}}>
+        <Row style={{ marginBottom: "50px" }}>
           <Col lg={6}>
             <img src={groupProgramming} alt="groupProgramming" />
           </Col>
@@ -24,7 +24,14 @@ const About = (props) => {
               What is TechXcelerate 3.0?
             </h1>
             <p className="techx-overview-text text-justify">
-            TechXcelerate, launched in April 2023, is a series of interactive sessions designed to empower first and second-year students with essential technical skills. These beginner-friendly sessions cover a wide range of topics, including Competitive Programming, web development, app development, data science, and more. Delivered by industry-leading professionals, TechXcelerate aims to provide students with the tips and tricks necessary to excel in various technical domains.
+              TechXcelerate, launched in April 2023, is a series of interactive
+              sessions designed to empower first and second-year students with
+              essential technical skills. These beginner-friendly sessions cover
+              a wide range of topics, including Competitive Programming, web
+              development, app development, data science, and more. Delivered by
+              industry-leading professionals, TechXcelerate aims to provide
+              students with the tips and tricks necessary to excel in various
+              technical domains.
             </p>
             <div className="counters-grid">
               <div>
@@ -97,17 +104,34 @@ const About = (props) => {
             >
               About
             </h1>
-            <h1
-              style={{
-                fontFamily: "Poppins, sans-serif",
-                fontWeight: 500,
-                marginBottom: "20px",
-              }}
+            <a
+              href="https://namespacecomm.in"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "inherit" }}
             >
-              The nameSpace Community ❤️
-            </h1>
+              <h1
+                style={{
+                  fontFamily: "Poppins, sans-serif",
+                  fontWeight: 500,
+                  marginBottom: "20px",
+                }}
+              >
+                The nameSpace Community ❤️
+              </h1>
+            </a>
+
             <p className="techx-overview-text text-justify">
-            The nameSpace Community is an organization of like-minded individuals who share a passion for technology and are committed to helping each other become the best developers they can be. Whether you're just starting out in your coding journey or a seasoned pro, our community is a place where you can connect with other tech enthusiasts, share your knowledge, and grow your skills. Founded in 2022, nameSpace has quickly grown to become one of the largest technical communities in Delhi-NCR and is racing towards further expansion and deeper penetration in its core segments.
+              The nameSpace Community is an organization of like-minded
+              individuals who share a passion for technology and are committed
+              to helping each other become the best developers they can be.
+              Whether you're just starting out in your coding journey or a
+              seasoned pro, our community is a place where you can connect with
+              other tech enthusiasts, share your knowledge, and grow your
+              skills. Founded in 2022, nameSpace has quickly grown to become one
+              of the largest technical communities in Delhi-NCR and is racing
+              towards further expansion and deeper penetration in its core
+              segments.
             </p>
             <div className="counters-grid">
               <div className="counter-item">
@@ -201,22 +225,20 @@ const About = (props) => {
             </div>
           </Col>
           <Col lg={6}>
-            <div style={{ height: "100%", display: "flex", alignItems: "center" }}>
+            <div
+              style={{ height: "100%", display: "flex", alignItems: "center" }}
+            >
               <video
                 style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 autoPlay
                 loop
                 muted
               >
-                <source
-                  src={nameSpaceVideo}
-                  type="video/mp4"
-                />
+                <source src={nameSpaceVideo} type="video/mp4" />
               </video>
             </div>
           </Col>
         </Row>
-
       </Container>
     </section>
   );

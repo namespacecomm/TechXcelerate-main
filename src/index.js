a01e12db4c9787da51ba8d9e558a1a17afa0b137
Pradeepto Sarkar <psrcofficial10@gmail.com>
2024-10-19 16:46:34 +0530

    vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 Kommunicate.init(process.env.REACT_APP_CHAT_TOKEN, {
   automaticChatOpenOnNavigation: true,
